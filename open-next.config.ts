4d47952d104ea09b2ecec721290ee24accc3d8cd
Kuche Kuche <kuchecoin@pm.me>
2025-09-16 10:47:18 +0300

    fix for cloudflare workers

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,6 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-
-export default defineCloudflareConfig();
+// import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+ 
+export default defineCloudflareConfig({
+//   incrementalCache: r2IncrementalCache,
+});
