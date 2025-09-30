49af3f223c82e14ca305ee331f8f333de7e20f67
evgenidefi <97796468+evgeniko@users.noreply.github.com>
2025-09-30 23:33:55 +0200

    add missing v4 deps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,14 @@ const wormholeConfig: config.WormholeConnectConfig = {
   ui: {
     title: 'Wormhole NTT UI',
     defaultInputs: {
-      fromChain: 'Solana',
-      toChain: 'BaseSepolia'
+      source: {
+        chain: 'Solana',
+        token: 'WSV',
+      },
+      destination: {
+        chain: 'BaseSepolia',
+        token: 'WSV',
+      },
     },
     // walletConnectProjectId: '', 
   },
